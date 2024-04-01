b3c0e0dba8671a46c108cf6f34f63bd3105218dc
Kondrich <123kondrich@gmail.com>
2024-04-01 16:55:00 +0300

    edited

--- task_manager/main/views.py
+++ task_manager/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request , "")
+    return render(request , "main/index.html")
 
 def about(request):
-    return HttpResponse("ABOUT")
+    return render(request,"main/about.html")
