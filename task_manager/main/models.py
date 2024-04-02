5675504b6b019cbe2c7682536e995bf9b23dfaa1
Kondrich <123kondrich@gmail.com>
2024-04-02 14:57:54 +0300

    edited

--- task_manager/main/models.py
+++ task_manager/main/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 
-class Task(models.Model):
-    title = models.IntegerField()
+# class Task(models.Model):
+#     title = models.IntegerField()
+#     name = models.CharField()
+    
+    
+   
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Projects\New_Project\venv
+set VIRTUAL_ENV=D:\Projects\Task_Manager\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
