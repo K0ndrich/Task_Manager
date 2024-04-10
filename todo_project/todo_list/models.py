a5fc30af1615926f40fab9e9481da04701ecc457
K0ndrich <123kondrich@gmail.com>
2024-04-10 13:48:14 +0300

    edited

--- todo_project/todo_list/models.py
+++ todo_project/todo_list/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+
 class ToDo(models.Model):
-    title = models.CharField(max_length=)
+    title = models.CharField("Название Задания", max_length=500)
+    is_complete = models.BooleanField("Завершено", default=False)
+
+    class Meta:
+        verbose_name = "Задание"
+        verbose_name_plural = "Задания"
+
+    def __str__(self):
+        return self.title
