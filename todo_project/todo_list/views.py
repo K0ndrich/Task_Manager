2a336256a33ffea739a57c1271ffab07ad28b8a3
K0ndrich <123kondrich@gmail.com>
2024-04-12 14:05:52 +0300

    edited

--- todo_project/todo_list/views.py
+++ todo_project/todo_list/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ToDo
+from django.views.decorators.http import require_http_methods
 
 
 def index(request):
@@ -9,3 +10,28 @@ def index(request):
         "todo_project/index.html",
         {"todo_list": todos, "title": "Главная Страница"},
     )
+
+
+# указываем только одни метод запроса, при котором будет работаь наше предсталвление
+@require_http_methods(["POST"])
+def add(request):
+    # берез значение поля title нашем модели ToDo
+    title = request.POST["title"]
+    # создаем новую запись в модели
+    todo = ToDo(title=title)
+    # сохраняем новосозданую запись
+    todo.save()
+    return redirect("index")
+
+
+def update(request, todo_id):
+    todo = ToDo.objects.get(pk=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+    return redirect("index")
+
+
+def delete(request, todo_id):
+    todo = ToDo.objects.get(pk=todo_id)
+    todo.delete()
+    return redirect("index")
